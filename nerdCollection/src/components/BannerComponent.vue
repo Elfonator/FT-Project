59770da6c03e9c231467484083b7e532af85e318
Elfonator <152084045+Elfonator@users.noreply.github.com>
2024-08-29 09:48:25 +0200

    Update BannerComponent.vue

--- nerdCollection/src/components/BannerComponent.vue
+++ nerdCollection/src/components/BannerComponent.vue
@@ -14,7 +14,7 @@ export default defineComponent({
 </script>
 
 <template>
-    <v-sheet class="flex-1-1-100 ma-0 pa-0" style="position: absolute; top: 30px; left: 0;" >
+    <v-sheet class="flex-1-1-1 ma-0 pa-0" style="position: relative; top: 30px; left: 0;" >
       <img :src="imagePath" alt="banner Image" width="100%"/>
     </v-sheet>
 </template>
