2440affc87faffb4226e8394bb8f3faefffa4699
Elfonator <152084045+Elfonator@users.noreply.github.com>
2024-08-29 10:03:44 +0200

    Update CarouselComponent.vue

--- nerdCollection/src/components/CarouselComponent.vue
+++ nerdCollection/src/components/CarouselComponent.vue
@@ -16,15 +16,24 @@ export default defineComponent({
 </script>
 
 <template>
-  <v-card class="align-self-center" style="position: relative; top:100px; width: 100%">
-    <v-card-title class="text-center font-weight-black" style="width: 900px">Lets Go!</v-card-title>
+  <v-card class="align-self-center" style="position: relative; width: 100%; height: 800px">
+    <v-card-title class="text-center font-weight-black comic-font d-flex justify-center align-center" style="width: 900px; height: 80px;">
+    NOW Available! </v-card-title>
     <!-- Vuetify Carousel component to display images -->
   <v-carousel>
-    <v-carousel-item  v-for="comicsItem in comics" :key="comicsItem.id" :src="comicsItem.imageUrl" cover></v-carousel-item>
+    <v-carousel-item
+      v-for="comicsItem in comics"
+      :key="comicsItem.id"
+      :src="comicsItem.imageUrl" contain></v-carousel-item>
   </v-carousel>
   </v-card>
 </template>
 
 <style scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=Bangers&display=swap');
+.comic-font {
+  font-family: 'Bangers', sans-serif;
+  font-size: 36px;
+  color: #ff5733;
+}
 </style>
